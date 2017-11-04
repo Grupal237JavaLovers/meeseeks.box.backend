cae4c4e05edc05414d30a2a24976a02133d7e504
Rares Serban <rares_serban96@yahoo.com>
2017-11-04 20:05:54 +0200

    Add proper not found exception and cors config.Also, do limiting for jobs in query.

--- src/main/java/meeseeks/box/controller/ProviderController.java
+++ src/main/java/meeseeks/box/controller/ProviderController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javassist.NotFoundException;
 import meeseeks.box.domain.ProviderEntity;
 import meeseeks.box.domain.SkillEntity;
+import meeseeks.box.exception.NotFoundException;
 import meeseeks.box.repository.ProviderRepository;
 import meeseeks.box.repository.SkillRepository;
 import meeseeks.box.service.UserService;
@@ -27,10 +26,8 @@ import meeseeks.box.service.UserService;
  */
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/provider")
 public class ProviderController {
-
     private final ProviderRepository providerRepository;
     private final UserService userService;
     private final SkillRepository skillRepo;
@@ -49,18 +46,18 @@ public class ProviderController {
         ProviderEntity provider = providerRepository.findOne(id);
 
         if (provider == null) {
-            throw new IllegalArgumentException("Provider not found");
+            throw new NotFoundException("Provider not found");
         }
 
         return provider;
     }
 
     @RequestMapping(value = "/get/{id}/skills/all", method = RequestMethod.GET)
-    public @ResponseBody Set<SkillEntity> getProviderSkillsById(@PathVariable("id") final Integer id) {
+    public @ResponseBody Set<SkillEntity> getProviderSkillsById(@PathVariable("id") final Integer id) throws NotFoundException {
         ProviderEntity provider = providerRepository.findOne(id);
 
         if (provider == null) {
-            throw new IllegalArgumentException("Provider not found");
+            throw new NotFoundException("Provider not found");
         }
 
         return provider.getSkills();
@@ -68,11 +65,11 @@ public class ProviderController {
 
     @RequestMapping(value = "/get/{id}/skills/add/{nameSkill}", method = RequestMethod.GET)
     public @ResponseBody Set<SkillEntity> addSkillsToProviderById(@PathVariable("id") final Integer id,
-                                                                  @PathVariable("nameSkill") final String skillName) {
+                                                                  @PathVariable("nameSkill") final String skillName) throws NotFoundException {
         ProviderEntity provider = providerRepository.findOne(id);
 
         if (provider == null) {
-            throw new IllegalArgumentException("Provider not found");
+            throw new NotFoundException("Provider not found");
         }
 
         SkillEntity skill = skillRepo.findByName(skillName).orElse(new SkillEntity(skillName));
@@ -86,7 +83,7 @@ public class ProviderController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public void registerProvider(@RequestBody @Valid ProviderEntity provider) {
-        LOGGER.info(provider.getUsername() + ": provider tried to register!");
+        LOGGER.info("Provider " + provider.getUsername() + " registers now ...");
 
         userService.saveUser(provider);
     }
